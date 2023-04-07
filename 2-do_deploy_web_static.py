d497511c890d2930a1b7d93b9a983304709f8c2f
mubrik <mubarakg4u@gmail.com>
2023-04-07 22:37:20 +0100

    ggg

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
 """ do dsome packing"""
-from fabric.api import env, put, run
+from fabric.api import env, put, run, local
+from datetime import datetime
 import os
 
 env.hosts = ['54.90.41.154', '52.91.160.206']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static
+    """
+    try:
+        # create the versions directory if it doesn't exist
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        # create the archive filename
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+        print("Packing web_static to {}".format(archive_path))
+        # create the archive
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except Exception as exc:
+        return None
 
 
 def do_deploy(archive_path):
@@ -13,7 +34,6 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        print("Executing task 'do_deploy'")
         # Upload the archive to the /tmp/
         put(archive_path, "/tmp/")
         # Uncompress
