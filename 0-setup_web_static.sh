91e4d745f78146447a1b20bfdc1e2a9e3fd665e4
mubrik <mubarakg4u@gmail.com>
2023-04-08 21:39:01 +0100

    fff

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
-# setup airbnb on a ubuntu 14.04 server
+# installs Nginx if not already installed
+# creates several directories as well as a sym link
+
+# Install Nginx if not already installed
 if [ ! -x "$(command -v nginx)" ]; then
   sudo service "$(sudo lsof -i :80 | grep LISTEN | awk '{print $1}' | head -n 1)" stop &/dev/null;
   sudo apt-get update
   sudo apt-get install -y nginx
 fi
-# create dirs
-sudo mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-# Set ownership of /data folder recursively to ubuntu
-sudo chown -hR ubuntu:ubuntu /data/
-# template html
+
+# Create required directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Create a fake HTML file to test Nginx
 echo "<html><head><title>Test HTML File</title></head><body><h1>This is a test.</h1></body></html>" | sudo tee /data/web_static/releases/test/index.html &>/dev/null
-# Create symbolic link
+
+# Create symbolic link to the test release
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-# change root html location
+
+# Set ownership to ubuntu user and group
+sudo chown -hR ubuntu:ubuntu /data/
+
 # Configure Nginx to serve content from the current release
-sudo sed -i '0,/^\(\s*\)server_name\s*.*$/s//\1server_name mb.tech www.mb.tech;/' /etc/nginx/sites-available/default
-sudo sed -i '0,/^\(\s*\)server_name mb.tech www.mb.tech;$/s//&\n\n\1location \/hbnb_static {\n\1\1alias \/data\/web_static\/current\/;\n\1\1autoindex off;\n\1}/' /etc/nginx/sites-available/default
-# verify nginx conf
+sudo sed -i '0,/^\(\s*\)server_name\s*.*$/s//\1server_name cheezaram.tech www.cheezaram.tech;/' /etc/nginx/sites-available/default
+sudo sed -i '0,/^\(\s*\)server_name cheezaram.tech www.cheezaram.tech;$/s//&\n\n\1location \/hbnb_static {\n\1\1alias \/data\/web_static\/current\/;\n\1\1autoindex off;\n\1}/' /etc/nginx/sites-available/default
+
 # Restart Nginx
 sudo service nginx stop &>/dev/null
 sudo service nginx start &>/dev/null
